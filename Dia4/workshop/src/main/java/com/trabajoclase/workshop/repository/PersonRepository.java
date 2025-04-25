c472c03aa19cb91bb13a4b82828135313e87995e
MariaGLi <lizarazoleal27@gmail.com>
2025-04-25 12:59:02 -0500

    feat :sparkles: SpringBoot.

---
+++ Dia4/workshop/src/main/java/com/trabajoclase/workshop/repository/PersonRepository.java
@@ -0,0 +1,22 @@
+package com.trabajoclase.workshop.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trabajoclase.workshop.domain.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+    List<Person> findByNameContains(String name);
+    List<Person> findByProgrammingLanguageEquals(String programmingLanguage);
+    // @Query("SELECT p FROM Person p WHERE p.name = ?1")
+    // List<Person> findByName(String name);
+    // List<Person> findByName(String name);
+    // List<Person> findByNameAndLastName(String name, String lastName);
+    // List<Person> findByNameOrLastName(String name, String lastName);
+    // List<Person> findByNameLike(String name);
+    // List<Person> findByNameNotLike(String name);
+    // List<Person> findByNameIn(List<String> names);
+    // List<Person> findByNameNotIn(List<String> names);
+
+}
